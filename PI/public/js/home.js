8cf31628cb8aee469985b738e36db4eb6984322d
João Almeida <toxicyszconta@gmail.com>
2025-01-22 08:58:56 -0400

    home atualizada, sem conflitos

--- PI/public/js/home.js
+++ PI/public/js/home.js
@@ -27,16 +27,28 @@ function proximaImagem(){
 
 /* configuração do Swiper para o segundo carrossel. */
 
-let swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-  });
+// let swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 3,
+//     spaceBetween: -30,
+//     loop: true,
+//     grabCursor: true,
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       breakpoints: {
+//         1300: {
+//           slidesPerView: 3,
+//         },
+//         // arrumar na responsividade
+//         600: {
+//             slidesPerView: 2,
+//             spaceBetween: 20,
+//             autoHeight: true,
+//             // direction: 'vertical'
+//           },
+//       }
+//   });
 
 /* botão de voltar */
 
