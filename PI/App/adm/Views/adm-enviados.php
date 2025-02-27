f8ce3e6aa44b913a3554c5366af5cf4dd66660da
João Almeida <toxicyszconta@gmail.com>
2025-02-27 10:49:59 -0400

    Começando a produção da tela

---
+++ PI/App/adm/Views/adm-enviados.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../public/css/adm-enviados.css">
+</head>
+<body>
+    <!-- navbar, tirar isso depois e deixar só como include. -->
+    <header>
+        <div class="listarU-logo">
+            <img src="../../../public/assets/img/Ativo 2.png " alt="logo tweeb">
+        </div> 
+    </header>
+
+    <!-- Barra de departamentos -->
+    <section class="listarU-departments-bar">
+        <div class="listarU-department"></div>
+    </section>
+
+    <div class="quantidade-pedidos">
+        <div class="pedidos-ui-card">
+            
+        </div>
+
+        <div class="pedidos-adicionar-envio">
+            <img src="../../../public/assets/img/plus-circle.svg" alt="">
+            <p>Adicionar Envio</p>
+        </div>
+    </div>
+</body>
+</html>
