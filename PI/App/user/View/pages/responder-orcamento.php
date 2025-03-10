92bb26607fe504c1da3fc514ff783cc331ea265b
Arthur <arthursantorogomes92@gmail.com>
2025-03-10 11:12:26 -0400

    começando task

---
+++ PI/App/user/View/pages/responder-orcamento.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../../public/css/responder-orcamento.css">
+    <title>Responder orçamento | Tweeb</title>
+</head>
+<body class="responder-orcamento-body">
+    <div class="responder-orcamento">
+        <div class="container-responder-orcamento-h1">
+            <h1 class="responder-orcamento-h1">
+                Responder Orçamento
+            </h1>
+        </div>
+        <div class="responder-orcamento-container1">
+            <div class="responder-orcamento-title">
+                <p class="responder-orcamento-p">
+                    Título
+                </p>
+                <input type="text" placeholder="Enter title" class="responder-orcamento-input">
+            </div>
+            <div class="responder-orcamento-enviado_por">
+                <p class="responder-orcamento-p">
+                    Enviada por
+                </p>
+                <input type="text" placeholder="Enter name" class="responder-orcamento-input">
+            </div>
+            <div class="responder-orcamento-responder_para">
+                <p class="responder-orcamento-p">
+                        Responder para
+                </p>
+                <input type="email" placeholder="Enter email" class="responder-orcamento-input">
+            </div>
+        </div>
+        <div class="responder-orcamento-data">
+            <p class="responder-orcamento-p">
+                    Data
+            </p>
+            <input type="date" class="responder-orcamento-input">
+        </div>
+        <div class="responder-orcamento-resposta">
+            <p class="responder-orcamento-p">
+                    Resposta
+            </p>
+            <input type="text" placeholder="Detalhe ao máximo a solicitação" class="responder-orcamento-input">
+        </div>
+        <input type="submit" class="responder-orcamento-input">
+    </div>
+</body>
+</html>
