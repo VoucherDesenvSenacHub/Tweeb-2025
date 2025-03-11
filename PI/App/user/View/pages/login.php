dd0eec8f52bc46871753517d57513a964a426a04
marcosg432 <mg9149303@gmail.com>
2025-03-11 10:49:02 -0400

    .

--- PI/App/user/View/pages/login.php
+++ PI/App/user/View/pages/login.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../../../../public/css/Login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
-<?php include __DIR__.'/../../../../includes/headernavb.php'; ?>
+ <?php include __DIR__.'/../../../../includes/headernavb.php'; ?>
 <body>
     <div class="forms">
         <form action="">
