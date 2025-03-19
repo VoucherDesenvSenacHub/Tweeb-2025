4e58bdccd51917f2e0ed08602eeac64e970ef147
AnaPatriciia <anapatriciaoliveira576@gmail.com>
2025-03-19 09:23:57 -0400

    ajustes de botoes, e responsividade

--- PI/App/user/View/pages/alterar-senha.php
+++ PI/App/user/View/pages/alterar-senha.php
@@ -49,8 +49,8 @@
 
 
             <div class="alterar-senha-botoes">
-                <button type="button" class="alterar-senha-cancelar">Cancelar</button>
-                <button type="submit" class="alterar-senha-salvar">Salvar alteração</button>
+                <button class="botao-alterar-senha" type="button" class="alterar-senha-cancelar">Cancelar</button>
+                <button class="botao-alterar-senha" type="submit" class="alterar-senha-salvar">Salvar alteração</button>
             </div>
     </div>
 </div>
