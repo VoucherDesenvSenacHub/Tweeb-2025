6c70d03cbb50d7f268d353a9fc701ad5fd588b4d
Leandro Oliveira <leandrocandido2222@gmail.com>
2025-05-29 10:43:25 -0400

    style: arrumando o css do cadastro

--- PI/App/user/View/pages/cadastro.php
+++ PI/App/user/View/pages/cadastro.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <?php include __DIR__.'/../../../../includes/headernavb.php'; ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="../../../../public/css/cadastro.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body class="Task2a-body">
