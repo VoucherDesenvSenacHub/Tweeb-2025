019bba5fdcf6036d2526387917d72b341fac744f
Arthur <arthursantorogomes92@gmail.com>
2025-06-23 11:10:33 -0400

    começando perfilADM

--- PI/App/adm/Views/pages/perfil-adm.php
+++ PI/App/adm/Views/pages/perfil-adm.php
@@ -1,120 +1,107 @@
+<?php
+require_once __DIR__ . '/../../Models/Funcionario.php';
 
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+if (!isset($_SESSION['funcionario'])) {
+    header('Location: login-funcionario.php');
+    exit();
+}
 
+function mascararCPF($cpf) {
+    $cpf = preg_replace('/\D/', '', $cpf);
+    if (strlen($cpf) !== 11) return '';
+    return '***.***.***-' . substr($cpf, 9, 2);
+}
+
+$funcionario = $_SESSION['funcionario'];
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-<?php include __DIR__.'/../../../../includes/headernavb.php'; ?>
+    <?php include __DIR__.'/../../../../includes/headernavb.php'; ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Perfil</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/Tweeb-2025/PI/public/css/perfil-usuario.css">
 </head>
 <body>
 
-<?php
-include __DIR__.'/../../../../includes/head-adm.php';
-?>
+<?php include __DIR__.'/../../../../includes/head-adm.php'; ?>
+<?php include __DIR__.'/../../../../includes/sidebar-Adm.php'; ?>
 
-<?php
-
-if(isset($_SESSION['funcionario'])){
-    include __DIR__.'/../../../../includes/sidebar-Adm.php';
-}
-else{
-    header("Location: login-funcionario.php");
-    exit();
-}
-
-?>
-
-<!-- Contêiner do Perfil -->
 <div class="perfil-tweeb">
     <div class="perfil-tweeb-container">
-        <button class="perfil-tweeb-editar">Oi Letícia, 👋🏼 </button>
+        <button class="perfil-tweeb-editar">Oi <?php echo htmlspecialchars($funcionario['nome']); ?>, 👋🏼 </button>
         
         <div class="perfil-tweeb-header">
             <div class="perfil-tweeb-imagem">
-                <img src="../../../../public/assets/img/Avatar.png" alt="Foto de perfil">
-                <button class="perfil-tweeb-editar-foto"><i class="fa-regular fa-pen-to-square" style="color: #4b5563;"></i></button>
+                <?php 
+                    $foto_perfil = !empty($funcionario['foto_perfil']) ? $funcionario['foto_perfil'] : 'imagem_padrao.png';
+                    $caminho_foto = strpos($foto_perfil, 'imagem_padrao.png') !== false ? 
+                        '/Tweeb-2025/PI/public/uploads/imagem_padrao.png' : 
+                        '/Tweeb-2025/PI/public/uploads/' . $foto_perfil;
+                ?>
+                <img src="<?php echo htmlspecialchars($caminho_foto); ?>" alt="Foto de perfil" class="foto-perfil">
+                
+                <label for="inputFotoPerfil" class="btn-upload-foto" title="Alterar foto">
+                    <i class="bi bi-cloud-arrow-up carregar-foto"></i>
+                </label>
+
+                <button class="perfil-tweeb-editar-foto" title="Editar">
+                    <i class="fa-regular fa-pen-to-square" style="color: #4b5563;"></i>
+                </button>
             </div>
             <div class="perfil-tweeb-info">
-                <h1 class="editar-perfil">Letícia Almeida</h1>
-                <p class="perfil-tweeb-email">leticia_almeida@gmail.com</p>
+                <h1><?php echo htmlspecialchars($funcionario['nome']); ?></h1>
+                <p class="perfil-tweeb-email"><?php echo htmlspecialchars($funcionario['email']); ?></p>
                 <div class="fio"></div>
             </div>
         </div>
 
-        <form class="perfil-tweeb-form">
+        <form class="perfil-tweeb-form" method="POST" enctype="multipart/form-data">
+            <input type="file" id="inputFotoPerfil" name="foto_perfil" accept="image/*" style="display: none;">
+
             <div class="perfil-tweeb-input-group">
                 <label for="primeiro-nome">Primeiro nome</label>
-                <input type="text" id="primeiro-nome" value="Igor">
+                <input type="text" id="primeiro-nome" name="nome" value="<?php echo htmlspecialchars($funcionario['nome']); ?>" readonly>
             </div>
 
             <div class="perfil-tweeb-input-group">
                 <label for="sobrenome">Sobrenome</label>
-                <input type="text" id="sobrenome2" value="Medeiros">
-            </div>
-            <div class="perfil-tweeb-input-group">
-                <label for="cpf">Matrícula*</label>
-                <input type="text" id="cpf" disabled value="123456789-12">
+                <input type="text" id="sobrenome" name="sobrenome" value="<?php echo htmlspecialchars($funcionario['sobrenome'] ?? ''); ?>" readonly>
             </div>
 
             <div class="perfil-tweeb-input-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" value="igormedeiros@gmail.com">
+                <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($funcionario['email']); ?>" readonly>
             </div>
 
             <div class="perfil-tweeb-input-group">
                 <label for="telefone">Telefone</label>
-                <input type="text" id="telefone" value="67 9 456789">
+                <input type="text" id="telefone" name="telefone" value="<?php echo htmlspecialchars($funcionario['telefone'] ?? ''); ?>" readonly>
+            </div>
+
+            <div class="perfil-tweeb-input-group">
+                <label for="matricula">Matrícula</label>
+                <input type="text" id="matricula" name="matricula" value="<?php echo htmlspecialchars($funcionario['matricula'] ?? ''); ?>" readonly>
             </div>
 
             <div class="perfil-tweeb-input-group">
                 <label for="cargo">Cargo</label>
-                <input type="text" id="cargo" value="funcionario">
+                <input type="text" id="cargo" name="cargo" value="<?php echo htmlspecialchars($funcionario['cargo'] ?? ''); ?>" readonly>
             </div>
-
-            <!-- <div class="perfil-tweeb-input-group">
-                <label for="bairro">Bairro</label>
-                <input type="text" id="bairro" value="Centro">
-            </div>
-
-            <div class="perfil-tweeb-input-group">
-                <label for="cep">CEP</label>
-                <input type="text" id="cep" value="798255-12">
-            </div>
-
-            <div class="perfil-tweeb-input-group">
-                <label for="estado">Estado</label>
-                <input type="text" id="estado" value="Mato Grosso do Sul">
-            </div> -->
-            
-        </form>
-    
-        
-               <!-- Campos para alterar senha -->
-               <div class="perfil-tweeb-input-group">
-                <h2>Alterar Senha</h2>
-                <label for="senha-atual">Senha Atual</label>
-                <input type="password" id="senha-atual" placeholder="Digite a senha atual">
-            </div>
-
-            <div class="perfil-tweeb-input-group">
-                <label for="nova-senha">Nova Senha</label>
-                <input type="password" id="nova-senha" placeholder="Digite a nova senha">
-            </div>
-
-            <div class="perfil-tweeb-input-group">
-                <label for="confirmar-senha">Confirmar Nova Senha</label>
-                <input type="password" id="confirmar-senha" placeholder="Confirme a nova senha">
-            </div>  
-
 
             <div class="perfil-tweeb-botoes">
                 <button type="button" class="perfil-tweeb-cancelar">Cancelar</button>
                 <button type="submit" class="perfil-tweeb-salvar">Salvar alteração</button>
             </div>
+        </form>
     </div>
+</div>
 
 <?php include __DIR__.'/../../../../includes/footer.php'; ?>
 </body>
