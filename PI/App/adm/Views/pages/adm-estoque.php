06c0647c176760ba05edfd12894774a5e54b5540
Igor Medeiros Gomes <igor54195846@aluno.ms.senac.br>
2025-07-01 11:13:41 -0400

    atualizacao

--- PI/App/adm/Views/pages/adm-estoque.php
+++ PI/App/adm/Views/pages/adm-estoque.php
@@ -22,6 +22,8 @@ session_start();
           <span><p>Visão Geral</p></span>
           <p><a href="adm-pedidos.php">Pedidos</a></p>
           <p><a href="adm-enviados.php">Enviados</a></p>
+          <a href="estoqueok.php"><p>Novo Produto</p></a>
+
         </div>  
 
     </div>
