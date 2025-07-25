aac276212e0819770c8fe7039aad295aa1d387d6
senabeatriz <beatrizsena.ux@gmail.com>
2025-07-25 17:43:16 -0400

    arthur, eu só resolvi o bug do login do adm nas páginas de estoqueok e ordemdeservico, e arrumei uma coisa do css de login

--- PI/App/DB/Database.php
+++ PI/App/DB/Database.php
@@ -3,9 +3,9 @@
 class Database{
     public $conn;
     public string $local="localhost";
-    public string $db="Tweeb25";
+    public string $db="140p2";
     public string $user="root";
-    public string $password="suporte@22";
+    public string $password="";
     public $table;
 
    
