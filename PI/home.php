8d2d401f9778a21fc89f5ea867e657fd8d431737
Arthur Santoro <arthursantorogomes92@gmail.com>
2025-07-26 14:51:22 -0400

    fix: erros nos icones dos cards dos produtos e links da home

--- PI/home.php
+++ PI/home.php
@@ -99,27 +99,27 @@ session_start();
                 </div>
             </div>
             <div class="categorias-card">
-                <a href="App/user/View/pages/Hardwares.php" class="card card1">
+                <a href="App/user/Controllers/ControllerProd/Departamento_Hardwares.php" class="card card1">
                     <img src="public/assets/img/phone-icon.png" alt="hardware">
                     <p>Hardwares</p>
                 </a>
-                <a href="App/user/View/pages/Perifericos.php" class="card card2">
+                <a href="App/user/Controllers/ControllerProd/Departamento_Perifericos.php" class="card card2">
                     <img src="public/assets/img/perifericos-icon.png" alt="periféricos">
                     <p>Periféricos</p>
                 </a>
-                <a href="App/user/View/pages/Energia.php" class="card card3">
+                <a href="App/user/Controllers/ControllerProd/Departamento_Energia.php" class="card card3">
                     <img src="public/assets/img/energia-icon.png" alt="periféricos">
                     <p>Energia</p>
                 </a>
-                <a href="App/user/View/pages/Audio.php" class="card card4">
+                <a href="App/user/Controllers/ControllerProd/Departamento_Audio.php" class="card card4">
                     <img src="public/assets/img/audio-icon.png" alt="periféricos">
                     <p>Aúdio</p>
                 </a>
-                <a href="App/user/View/pages/Computadores.php" class="card card5">
+                <a href="App/user/Controllers/ControllerProd/Departamento_Computadores.php" class="card card5">
                     <img src="public/assets/img/computadores-icon.png" alt="periféricos">
                     <p>Computadores</p>
                 </a>
-                <a href="App/user/View/pages/Games.php" class="card card6">
+                <a href="App/user/Controllers/ControllerProd/Departamento_Games.php" class="card card6">
                     <img src="public/assets/img/jogos-icon.png" alt="periféricos">
                     <p>Jogos</p>
                 </a>
